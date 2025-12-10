415e5b3d12dcd6ade6a41172092991ef98b06758
aromayaT <gianauratime@gmail.com>
2025-12-10 22:55:34 +0100

    Update MAZE.py

--- MAZE.py
+++ MAZE.py
@@ -6,30 +6,50 @@ import networkx as nx
 def creer_labyrinthe(largeur, hauteur, diff):
     """Créer un labyrinthe parfait fonctionnel.
     """
+
     laby = nx.Graph()
-    arete  = list()
-    #been_there = []
+    #arete  = list()
+    been_there = []
     for i in range(largeur):
         for j in range(hauteur):
             laby.add_node(j * largeur + i)
-    print(laby.nodes)
-    depart = random.choice(laby.nodes)
-    if depart:
-        been_there.append(depart)
-        if been_there == False:
-            for voisin in i, j:
-                if voisin > j+1 or voisin < j or voisin > i+1 or voisin < i:
-                    return None
-                else: 
-                    s = j+1
-                    n = j-1
-                    e = i+1
-                    w = i-1
-                    voisins = [n, e, w, s]
-                    for possible in range(voisins.random):
-                        depart = possible
+    print("Nœuds créés :", list(laby.nodes))
+    depart = random.choice(list(laby.nodes))
+    been_there.append(depart)
+    
+    x = depart % largeur
+    y = depart // largeur
+    
+    while len(been_there) < largeur * hauteur:
+        voisins = []
+        if y > 0:
+            voisins.append(((x, y-1), (y-1) * largeur + x))
+        if y < hauteur - 1:
+            voisins.append(((x, y+1), (y+1) * largeur + x))
+        if y > 0:
+            voisins.append(((x+1, y), y * largeur + (x+1)))
+        if x < largeur - 1:
+            voisins.append(((x+1, y), y * largeur + (x+1)))
+        voisins = [v for v in voisins if v[1] not in been_there]
+        
+        if voisins:
+            (xn, yn), new_id = random.choice(voisins)
+            laby.add_edge(depart, new_id)
+            been_there.append(new_id)
+            x, y = xn, yn
+            depart = new_id
         else:
-            return None
+            depart = been_there[-2]
+            been_there.pop()
+            x = depart % largeur
+            y = depart // largeur
+
+        (x1, y1), new_id = random.choice(voisins)
+        laby.add_edge(depart, new_id)
+        been_there.append(new_id)
+        x, y = x1, y1
+        depart = new_id
+    return laby
                         
 
 
@@ -103,3 +123,37 @@ creer_labyrinthe(4, 3, 30)
     m = nx.adjacency_matrix(Labyrinthe)
 
     print(m)"""
+
+"""ancien code :
+#importation des bibliothèques
+import random 
+import networkx as nx
+
+
+def creer_labyrinthe(largeur, hauteur, diff):
+    """Créer un labyrinthe parfait fonctionnel.
+    """
+    laby = nx.Graph()
+    arete  = list()
+    #been_there = []
+    for i in range(largeur):
+        for j in range(hauteur):
+            laby.add_node(j * largeur + i)
+    print(laby.nodes)
+    depart = random.choice(laby.nodes)
+    if depart:
+        been_there.append(depart)
+        if been_there == False:
+            for voisin in i, j:
+                if voisin > j+1 or voisin < j or voisin > i+1 or voisin < i:
+                    return None
+                else: 
+                    s = j+1
+                    n = j-1
+                    e = i+1
+                    w = i-1
+                    voisins = [n, e, w, s]
+                    for possible in range(voisins.random):
+                        depart = possible
+        else:
+            return None"""
